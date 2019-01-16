ea9bc384875fe8e2915d91b7996b406f88691599
rahmo Alzahrani <abdulrahman.alzahrani@morningstar.com>
2019-01-16 11:51:18 -0600

    blocks

---
+++ client/src/components/Blocks.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Blocks extends Component {
+    //state = {blocks : []}; TODO
+    state = {
+        blocks: [
+            {hash: 'hash-one'},
+            {hash: 'hash-two'},
+            {hash: 'hash-three'}
+        ]
+    };
+
+    componentDidMount() {
+        fetch('https://www.localhost:3000/api/blocks')
+            .then(response => response.json())
+            .then(json => this.setState({blocks: json}))
+    }
+
+    render() {
+        return (<div>
+            <h3> Blocks </h3>{
+            this.state.blocks.map(block => {
+                return (
+                    <div key={block.hash}> {block.hash} </div>
+                )
+            })}
+        </div>)
+    }
+}
+
+export default Blocks;
